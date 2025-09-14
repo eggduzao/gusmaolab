1e28e8015663261d92e9a6056307642c04331b5c
eggduzao <eggduzao@gmail.com>
2025-09-14 15:16:01 -0300

    initial commit of novel structure

---
+++ docs/org/website_tree_v3.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+mkdir -p /Users/egg/Projects/GusmaoLab
+touch /Users/egg/Projects/GusmaoLab/LICENSE
+touch /Users/egg/Projects/GusmaoLab/README.md
+touch /Users/egg/Projects/GusmaoLab/Makefile
+touch /Users/egg/Projects/GusmaoLab/pelicanconf.py
+touch /Users/egg/Projects/GusmaoLab/publishconf.py
+touch /Users/egg/Projects/GusmaoLab/requirements.txt
+touch /Users/egg/Projects/GusmaoLab/environment.yml
+touch /Users/egg/Projects/GusmaoLab/.editorconfig
+mkdir -p /Users/egg/Projects/GusmaoLab/.git
+touch /Users/egg/Projects/GusmaoLab/.gitignore
+touch /Users/egg/Projects/GusmaoLab/.pre-commit-config.yaml
+touch /Users/egg/Projects/GusmaoLab/.lycheeignore
+touch /Users/egg/Projects/GusmaoLab/.npmrc
+touch /Users/egg/Projects/GusmaoLab/package.json
+mkdir -p /Users/egg/Projects/GusmaoLab/scripts
+touch /Users/egg/Projects/GusmaoLab/scripts/gen_social_og.py
+touch /Users/egg/Projects/GusmaoLab/scripts/validate_leaderboard.js
+touch /Users/egg/Projects/GusmaoLab/scripts/build_viz_svg.py
+touch /Users/egg/Projects/GusmaoLab/scripts/gen_cover_thumbs.py
+touch /Users/egg/Projects/GusmaoLab/scripts/gen_search_index.py
+touch /Users/egg/Projects/GusmaoLab/scripts/profile_metrics_snapshot.py
+touch /Users/egg/Projects/GusmaoLab/tasks.py
+touch /Users/egg/Projects/GusmaoLab/clean.sh
+mkdir -p /Users/egg/Projects/GusmaoLab/backup
+mkdir -p /Users/egg/Projects/GusmaoLab/backup/2025-09-08
+mkdir -p /Users/egg/Projects/GusmaoLab/releases
+mkdir -p /Users/egg/Projects/GusmaoLab/releases/p1
+mkdir -p /Users/egg/Projects/GusmaoLab/releases/p2
+mkdir -p /Users/egg/Projects/GusmaoLab/releases/p3
+mkdir -p /Users/egg/Projects/GusmaoLab/releases/p4
+mkdir -p /Users/egg/Projects/GusmaoLab/releases/p5
+mkdir -p /Users/egg/Projects/GusmaoLab/.github
+mkdir -p /Users/egg/Projects/GusmaoLab/.github/workflows
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/build_deploy.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/link_check.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/artifact_keep5.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/pandoc_cv.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/nightly_resource_metrics.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/lighthouse_ci.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/pa11y_axe.yml
+touch /Users/egg/Projects/GusmaoLab/.github/workflows/visual_regression.yml
+mkdir -p /Users/egg/Projects/GusmaoLab/content
+mkdir -p /Users/egg/Projects/GusmaoLab/content/blog
+touch /Users/egg/Projects/GusmaoLab/content/blog/2025-08-02-first-post.md
+mkdir -p /Users/egg/Projects/GusmaoLab/content/blog/_archetypes
+touch /Users/egg/Projects/GusmaoLab/content/blog/_archetypes/tutorial.md
+touch /Users/egg/Projects/GusmaoLab/content/blog/_archetypes/deep-dive.md
+touch /Users/egg/Projects/GusmaoLab/content/blog/_archetypes/release-notes.md
+touch /Users/egg/Projects/GusmaoLab/content/blog/_archetypes/sunday-log.md
+mkdir -p /Users/egg/Projects/GusmaoLab/content/pages
+touch /Users/egg/Projects/GusmaoLab/content/pages/about.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/projects.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/contact.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/learning.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/teaching.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/team_members.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/resources.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/styleguide.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/press.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/brand.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/talks.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/publications.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/privacy.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/terms.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/licenses.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/challenges.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/search.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/thanks.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/404.md
+touch /Users/egg/Projects/GusmaoLab/content/pages/410.md
+mkdir -p /Users/egg/Projects/GusmaoLab/content/data
+touch /Users/egg/Projects/GusmaoLab/content/data/projects.json
+touch /Users/egg/Projects/GusmaoLab/content/data/resources.json
+touch /Users/egg/Projects/GusmaoLab/content/data/leaderboard.sample.json
+touch /Users/egg/Projects/GusmaoLab/content/data/leaderboard.schema.json
+touch /Users/egg/Projects/GusmaoLab/content/data/profile.json
+touch /Users/egg/Projects/GusmaoLab/content/data/search_synonyms.json
+mkdir -p /Users/egg/Projects/GusmaoLab/content/extra
+touch /Users/egg/Projects/GusmaoLab/content/extra/CNAME
+touch /Users/egg/Projects/GusmaoLab/content/extra/robots.txt
+touch /Users/egg/Projects/GusmaoLab/content/extra/security.txt
+touch /Users/egg/Projects/GusmaoLab/content/extra/humans.txt
+mkdir -p /Users/egg/Projects/GusmaoLab/content/images
+mkdir -p /Users/egg/Projects/GusmaoLab/content/images/posts
+mkdir -p /Users/egg/Projects/GusmaoLab/content/images/team
+mkdir -p /Users/egg/Projects/GusmaoLab/content/images/brand
+mkdir -p /Users/egg/Projects/GusmaoLab/content/images/social
+mkdir -p /Users/egg/Projects/GusmaoLab/content/images/favicons
+mkdir -p /Users/egg/Projects/GusmaoLab/content/downloads
+mkdir -p /Users/egg/Projects/GusmaoLab/content/downloads/kits
+mkdir -p /Users/egg/Projects/GusmaoLab/content-pt
+mkdir -p /Users/egg/Projects/GusmaoLab/content-pt/blog
+mkdir -p /Users/egg/Projects/GusmaoLab/content-pt/pages
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/sobre.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/projetos.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/contato.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/aprendizado.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/ensino.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/equipe.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/recursos.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/privacidade.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/termos.md
+touch /Users/egg/Projects/GusmaoLab/content-pt/pages/licencas.md
+mkdir -p /Users/egg/Projects/GusmaoLab/static
+mkdir -p /Users/egg/Projects/GusmaoLab/static/css
+touch /Users/egg/Projects/GusmaoLab/static/css/tokens.css
+touch /Users/egg/Projects/GusmaoLab/static/css/components.css
+touch /Users/egg/Projects/GusmaoLab/static/css/print.css
+touch /Users/egg/Projects/GusmaoLab/static/css/dark.css
+mkdir -p /Users/egg/Projects/GusmaoLab/static/js
+touch /Users/egg/Projects/GusmaoLab/static/js/lunr.min.js
+touch /Users/egg/Projects/GusmaoLab/static/js/search.js
+touch /Users/egg/Projects/GusmaoLab/static/js/leaderboard.js
+touch /Users/egg/Projects/GusmaoLab/static/js/theme-toggle.js
+touch /Users/egg/Projects/GusmaoLab/static/js/giscus.js
+mkdir -p /Users/egg/Projects/GusmaoLab/static/fonts
+mkdir -p /Users/egg/Projects/GusmaoLab/static/img
+mkdir -p /Users/egg/Projects/GusmaoLab/static/img/social
+mkdir -p /Users/egg/Projects/GusmaoLab/static/img/brand
+mkdir -p /Users/egg/Projects/GusmaoLab/static/img/placeholders
+touch /Users/egg/Projects/GusmaoLab/static/manifest.webmanifest
+mkdir -p /Users/egg/Projects/GusmaoLab/themes
+mkdir -p /Users/egg/Projects/GusmaoLab/themes/Flex
+mkdir -p /Users/egg/Projects/GusmaoLab/themes/GusmaoLab
+mkdir -p /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/base.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/index.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/page.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/article.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/archives.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/tag.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/series.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/search.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/challenges.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/resources.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/projects.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/publications.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/talks.html
+touch /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/templates/styleguide.html
+mkdir -p /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/static
+mkdir -p /Users/egg/Projects/GusmaoLab/themes/GusmaoLab/macros
+mkdir -p /Users/egg/Projects/GusmaoLab/docs
+touch /Users/egg/Projects/GusmaoLab/docs/publishing.md
+touch /Users/egg/Projects/GusmaoLab/docs/design-system.md
+touch /Users/egg/Projects/GusmaoLab/docs/a11y-checklist.md
+touch /Users/egg/Projects/GusmaoLab/docs/challenges-overview.md
+mkdir -p /Users/egg/Projects/GusmaoLab/docs/challenges-repo
+touch /Users/egg/Projects/GusmaoLab/docs/challenges-repo/CONTRIBUTING.md
+touch /Users/egg/Projects/GusmaoLab/docs/challenges-repo/PULL_REQUEST_TEMPLATE.md
+touch /Users/egg/Projects/GusmaoLab/docs/challenges-automation.md
+mkdir -p /Users/egg/Projects/GusmaoLab/docs/seo
+touch /Users/egg/Projects/GusmaoLab/docs/seo/search-console-proofshots.md
+touch /Users/egg/Projects/GusmaoLab/docs/seo/structured-data-notes.md
+touch /Users/egg/Projects/GusmaoLab/docs/build-env.md
+touch /Users/egg/Projects/GusmaoLab/docs/search-tests.md
+touch /Users/egg/Projects/GusmaoLab/docs/hyperglam-a11y.md
+touch /Users/egg/Projects/GusmaoLab/docs/hyperglam-metrics.md
+mkdir -p /Users/egg/Projects/GusmaoLab/docs/org
+touch /Users/egg/Projects/GusmaoLab/docs/org/website_todo_v1.txt
+touch /Users/egg/Projects/GusmaoLab/docs/org/website_todo_v2.txt
+touch /Users/egg/Projects/GusmaoLab/docs/org/website_todo_v3.txt
+mkdir -p /Users/egg/Projects/GusmaoLab/data
+touch /Users/egg/Projects/GusmaoLab/data/search_index.json
+touch /Users/egg/Projects/GusmaoLab/data/build_times.csv
+mkdir -p /Users/egg/Projects/GusmaoLab/data/viz_cache
+mkdir -p /Users/egg/Projects/GusmaoLab/publications
+touch /Users/egg/Projects/GusmaoLab/publications/publications.bib
+touch /Users/egg/Projects/GusmaoLab/publications/style.csl
+mkdir -p /Users/egg/Projects/GusmaoLab/publications/templates
+touch /Users/egg/Projects/GusmaoLab/publications/templates/pub_list.md
+mkdir -p /Users/egg/Projects/GusmaoLab/cv
+touch /Users/egg/Projects/GusmaoLab/cv/cv.md
+touch /Users/egg/Projects/GusmaoLab/cv/template.tex
+mkdir -p /Users/egg/Projects/GusmaoLab/resources
+mkdir -p /Users/egg/Projects/GusmaoLab/resources/thumbnails
+mkdir -p /Users/egg/Projects/GusmaoLab/resources/samples
+mkdir -p /Users/egg/Projects/GusmaoLab/brand
+touch /Users/egg/Projects/GusmaoLab/brand/gusmaolab-logo.svg
+mkdir -p /Users/egg/Projects/GusmaoLab/brand/project-logos
+mkdir -p /Users/egg/Projects/GusmaoLab/brand/project-logos/bloom
+touch /Users/egg/Projects/GusmaoLab/brand/project-logos/bloom/bloom-mark.svg
+touch /Users/egg/Projects/GusmaoLab/brand/project-logos/bloom/bloom-wordmark.svg
+mkdir -p /Users/egg/Projects/GusmaoLab/brand/project-logos/stainalyzer
+touch /Users/egg/Projects/GusmaoLab/brand/project-logos/stainalyzer/stainalyzer-mark.svg
+touch /Users/egg/Projects/GusmaoLab/brand/project-logos/stainalyzer/stainalyzer-wordmark.svg
+mkdir -p /Users/egg/Projects/GusmaoLab/brand/kit
+touch /Users/egg/Projects/GusmaoLab/brand/kit/gusmaolab-brand-kit.zip
+mkdir -p /Users/egg/Projects/GusmaoLab/viz
+mkdir -p /Users/egg/Projects/GusmaoLab/viz/data
+mkdir -p /Users/egg/Projects/GusmaoLab/viz/svg
+mkdir -p /Users/egg/Projects/GusmaoLab/hyperglam
+mkdir -p /Users/egg/Projects/GusmaoLab/hyperglam/nucleus
+touch /Users/egg/Projects/GusmaoLab/hyperglam/nucleus/data/regions.json
+touch /Users/egg/Projects/GusmaoLab/hyperglam/nucleus/nucleus.svg
+touch /Users/egg/Projects/GusmaoLab/hyperglam/nucleus/nucleus.js
+mkdir -p /Users/egg/Projects/GusmaoLab/hyperglam/sandboxes
+mkdir -p /Users/egg/Projects/GusmaoLab/hyperglam/sandboxes/kmeans
+touch /Users/egg/Projects/GusmaoLab/hyperglam/sandboxes/kmeans/worker.js
+touch /Users/egg/Projects/GusmaoLab/hyperglam/sandboxes/kmeans/demo.html
+mkdir -p /Users/egg/Projects/GusmaoLab/hyperglam/sandboxes/common
+mkdir -p /Users/egg/Projects/GusmaoLab/hyperglam/cards
+touch /Users/egg/Projects/GusmaoLab/hyperglam/cards/tilt.js
+mkdir -p /Users/egg/Projects/GusmaoLab/output
