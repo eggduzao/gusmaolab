ddedf6684ba952bd78119be38960e5fcde1d71b5
eggduzao <eggduzao@gmail.com>
2026-01-29 16:33:39 -0300

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -95,7 +95,6 @@ GOOGLE_ANALYTICS = ""
 # Default pagination
 DEFAULT_PAGINATION = 10
 
-
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 ######################################
